2f1ae55d153f2a74c39657b82c8da7a51c62345c
plkong <379338796@QQ.COM>
2018-03-18 12:18:17 +0800

    update

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,8 +3,10 @@
     <el-row style="padding-top: 10px">
       <el-col>
         <el-button type="success" @click="generateHtml()">生成静态网站</el-button>
+      
+        <el-button type="success"><a href="http://localhost:8081/pages/goods/2.html" target="_blank">打开页面</a></el-button>
+        
       </el-col>
-
     </el-row>
 
   </div>
@@ -18,13 +20,22 @@
     },
     methods: {
       generateHtml(){
+        let self = this;
         this.$axios
         .get("api/generate")
         .then(response => {
-           console.log(response);
+           self.$message({
+              showClose: true,
+              message: "生成成功",
+              type: "success"
+            });
         })
         .catch(function(response) {
-          console.log(response);
+          self.$message({
+              showClose: true,
+              message: "生成失败",
+              type: "error"
+            });
         });
       }
     }
